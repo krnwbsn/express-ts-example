2cd7caf8155ad9416fa08730e6148551988e3bfb
krina <krinawibisana@gmail.com>
2021-01-29 22:24:44 +0700

    CRUD implementation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import bodyParser from "body-parser";
+import { itemsRouter } from "./routers/itemsRouter";
+import { errorHandler } from "./middleware/error";
+import { notFoundHandler } from "./middleware/not-found";
 
 dotenv.config();
 
@@ -16,7 +20,14 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api/menu/items", itemsRouter);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
